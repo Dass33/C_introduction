b615ee0e90fcc8d4104ec29301a458963b33cc02
Dass33 <94531241+Dass33@users.noreply.github.com>
2023-10-21 22:20:53 +0200

    Update AlphabeticalSort.cpp

--- AlphabeticalSort/AlphabeticalSort/AlphabeticalSort.cpp
+++ AlphabeticalSort/AlphabeticalSort/AlphabeticalSort.cpp
@@ -3,9 +3,8 @@
 #include <ctype.h>
 
 #define MAX_SIZE 2048
-#define ASCII_MAX 128
 #define ASCII_Start_Alphabet 96
-#define ASCII_End_Alphabet 123
+#define ASCII_End_ALPHABET 123
 //constants declaration
 
 char* sort(char* input, int SizeInput); //delcaration of sort function
@@ -13,7 +12,7 @@ char* sort(char* input, int SizeInput); //delcaration of sort function
 int main()
 {
     char input[] = "Ahoj Nomade, jak pak se mas? :)"; //input for our sort funcion
-    int SizeInput = sizeof(input); //declaration of size
+    int SizeInput = sizeof(input) -1; //declaration of size withnout the null pointer
     if (MAX_SIZE < SizeInput || SizeInput < 1) //check if the input is of a reasonable size
     {
         return -1; //error code
@@ -21,7 +20,7 @@ int main()
 
     char* sortedOutput = sort(input, SizeInput); //call the sort function and save the pointer to the start
     if (sortedOutput != NULL) //if there are any sorted data print them
-    { 
+    {
         printf("sorted by ASCII chart: %s", sortedOutput); //printing characters till is gets to the null
         free(sortedOutput); // Free the allocated memory
         //this is important step that is often missed
@@ -30,26 +29,25 @@ int main()
     return 0; //no errors
 }
 
-char* sort(char* input, int SizeInput) 
+char* sort(char* input, int SizeInput)
 {
     char* output;
-    output = (char*)calloc(SizeInput, sizeof(char)); //alocated memory
-    
-    
-    if (output == NULL) 
+    output = (char*)calloc(SizeInput +1, sizeof(char)); //alocated memory with added size for the null pointer
+
+
+    if (output == NULL)
     {
         return NULL; // Return NULL in case of memory allocation failure
     }
 
     int OutputNum = 0;
 
-    for (int ASCII_NUM = 0; ASCII_NUM < ASCII_MAX; ASCII_NUM++) //looping through all ascii characters
+    for (int ASCII_NUM = ASCII_Start_Alphabet; ASCII_NUM <= ASCII_End_ALPHABET; ASCII_NUM++) //looping through all ascii characters
     {
         for (int inputNum = 0; inputNum < SizeInput; inputNum++) //looping through all of input characters 
         {
-            if (input[inputNum] == ASCII_NUM && ASCII_Start_Alphabet < tolower(input[inputNum]) && tolower(input[inputNum]) < ASCII_End_Alphabet)
-            //if the current input char is same as current ascii num
-            //and if it is in range where is alphabet in ascii
+            if (tolower(input[inputNum]) == ASCII_NUM)
+                //if the current input char is same as current ascii num
             {
                 output[OutputNum] = tolower(input[inputNum]); //write into an output array current input char
                 OutputNum++; //increment by size of an char
